67c7ff9404ad3173ce65ebbf23e8e2ec8cce5458
paimoe <me@paimoe.com>
2019-04-05 22:37:15 +0800

    More stuff, better label resolving

--- parser.py
+++ parser.py
@@ -37,7 +37,8 @@ def strip_fully_surrounded(s):
     #print('Converting', s, ' => ', s[1:-1])
     return s[1:-1]
 
-
+def resolver(s, lookup):
+    pass
 
 class Item(object):
     def __init__(self, a, b):
@@ -52,6 +53,8 @@ class Item(object):
             return '{a} . {b}'.format(a=self.a, b=self.b)
         elif n == 'OR':
             return '{a} + {b}'.format(a=self.a, b=self.b)
+        elif n == 'NOT':
+            return '~{a}'.format(a=self.a)
         else:
             return self.__repr__()
 
@@ -59,7 +62,7 @@ class AND(Item):
     def __init__(self, a, b):
         super().__init__(a,b)
     def update(self, **kwargs):
-        print(self.a, self.b, '5 on it')
+        #print(self.a, self.b, '5 on it')
         if str(self.a).endswith(kwargs['ph']):
             self.a = kwargs[self.a]
         elif str(self.b).endswith(kwargs['ph']):
@@ -70,12 +73,20 @@ class AND(Item):
         #print('kz', kz)
         k1 = str(self.a)
         k2 = str(self.b)
+        ph_key_match = r"(\$ph\d+)"
         if ph is not None:
-            if k2.startswith('$ph'):
-                # Check ph stack, then use
-                k2 = ph[k2].label()
-            if k1.startswith('$ph'):
-                k1 = ph[k1].label()
+            while has_ph(k2):
+                # Match and replace
+                for mnum, match in enumerate(re.finditer(ph_key_match, k2, re.MULTILINE)):
+                    g = match.group(0)
+                    newlabel = '({})'.format(ph[g].label())
+                    k2 = strip_fully_surrounded(re.sub(ph_key_match, newlabel, k2))
+            while has_ph(k1):
+                # Match and replace
+                for mnum, match in enumerate(re.finditer(ph_key_match, k1, re.MULTILINE)):
+                    g = match.group(0)
+                    newlabel = '({})'.format(ph[g].label())
+                    k1 = strip_fully_surrounded(re.sub(ph_key_match, newlabel, k1))
 
         return np.logical_and(kz[k1], kz[k2])
 class OR(Item):
@@ -98,9 +109,18 @@ class OR(Item):
                     g = match.group(0)
                     newlabel = '({})'.format(ph[g].label())
                     k1 = strip_fully_surrounded(re.sub(ph_key_match, newlabel, k1))
-        #print('kz', type(kz), kz[self.x], kz[self.y])
+        #print('OR kz', type(kz), k1, ' + ', k2)
+        #print(kz)
         # kz: dict of key: binary value, {'q': 0, 'p': 1, 's': 0}
-        return np.logical_or(kz[k1], kz[k2])
+        zb = np.logical_or(kz[k1], kz[k2])
+        #print('Returning ', zb)
+        return zb
+class NOT(Item):
+    def __init__(self, a):
+        super().__init__(a.lstrip('~'), b='')
+    def compute(self, kz, ph=None):
+        k1 = str(self.a)
+        return np.logical_not(kz[k1])
 
 class Parse(object):
 
@@ -143,12 +163,15 @@ class Parse(object):
             raise Exception("Unbalanced brackets")
         #t = t.replace('}', ' } ').replace('{', ' { ').replace('=', ' = ').replace("\n", " ")
 
+        # Run clean first to match all the simple NOTs
+        self.s = self.clean(self.s)
+
         while contains('(', self.s):
             self.s = self.parseholder(self.s)
 
         self.s = self.clean(self.s)
 
-        print('Parsed: {o} => {s}'.format(o=self._original, s=self.s))
+        print('=== Parsed: {o} => {s}'.format(o=self._original, s=self.s))
         #print(self.ph_stack)
 
     def clean(self, s):
@@ -162,53 +185,90 @@ class Parse(object):
         #if re.match()
         _or = r"(\w|\$ph.?)\s?\+\s?(\w|\$ph.?)"
         _and = r"(\w|\$ph.?)\s?\.\s?(\w|\$ph.?)"
-        if re.match(_or, s):
+        _not = r"(~\w)+"
+        #print('our s', repr(s), _not, re.search(_or, s))
+        # Just simple NOTs. parsed will be $ph1 + ~$ph2 which we can get later
+        if re.search(_not, s):
+            #print('checking not')
+            m = self.match(_not, s, simple=False)
+            for k in m:
+                # Set placeholder?
+                self.ph_id += 1
+                phkey = '$ph{}'.format(self.ph_id)
+                self.ph_stack[phkey] = NOT(k)
+                srch = re.search(k, s).start()
+
+                s = s[0:srch] + phkey + s[srch+len(k):]
+            #print('returning s', s, self.ph_stack)
+            #assert False
+            return s
+
+            # Replace the NOTs with the placeholders
+        elif re.search(_or, s):
             m = self.match(_or, s)
+            #assert False
+            #print('checkin or', m, self.ph_stack)
+            #assert False
             return OR(*m)#'OR:{},{}'.format(*m)
             #print('Match vars {s}={m}'.format(s=s, m=m))
         elif re.match(_and, s):
+            #print('checkin and')
             m = self.match(_and, s)
             return AND(*m)#'AND:{},{}'.format(*m)
         return s
 
-    def match(self, r, s):
+    def match(self, r, s, simple=True):
+        g = []
+        #print('s', r, s)
         for mnum, match in enumerate(re.finditer(r, s, re.MULTILINE)):
-            #print('Match {mnum} was found: {g}'.format(mnum=mnum, g=match.groups()))
+            #print('Match {mnum} on s={s} was found: {g}'.format(mnum=mnum, s=s, g=match.groups()))
             # use str for now until return an object
-            return match.groups()
+            if simple:
+                return match.groups()
+            else:
+                g.append(match.group(0))
+        return tuple(g)
 
     def components(self):
         # Return basically all the placeholders in rev order or well some order
         # tuple pair (label, object)
+        # Think its being run for every row? so we'll need to cache it? oh nah can't
         _or = r"(\w|\$ph.?)\s?\+\s?(\w|\$ph.?)"
         _and = r"(\w|\$ph.?)\s?\.\s?(\w|\$ph.?)"
         r = []
-        ph_key_match = r"(\$ph.+)"
-        #print('stack', self.ph_stack)
-        for key, c in sorted(self.ph_stack.items(), reverse=True):
+        ph_key_match = r"(\$ph\d+)"
+        #print('components() stack', self.ph_stack, self.s.label())
+        #self.ph_stack['$ph{}'.format(len(self.ph_stack) + 1)] = self.s
+        all_labels = {}
+        for key, c in sorted(self.ph_stack.items(), reverse=False):
             n = type(c).__name__
             label = c.label()
+            #print('=n', n, c, label)
             while True:
                 if '$ph' not in label:
+                    all_labels[key] = label
                     break
                 else:
-                    # Get it out bro
-                    matches = self.match(_and, label)
-                    #print('matchez', matches)
+                    # uh do i need this or just use resolve_label
+                    regex_use = _and if n == 'AND' else _or
+                    matches = self.match(regex_use, label)
+                    #print('matchez', matches, label)
                     for phkey in matches:
                         if phkey.startswith('$ph'):
-                            label = re.sub(ph_key_match, '(' + self.ph_stack[phkey].label() + ')', label)
-                            #print('match', matches, label)
-                    if n == 'AND':
-                        # Also update object with new value? do earlier better
-                        pass
-                        #c.update(ph='ph2', ph2=self.ph_stack[phkey])
-                        #print('AND', c.compute({'q': True, 'p':True, '$ph2': True}))
-                    elif n == 'OR':
-                        pass
+                            old_label = label
+                            #print('NNNNNN',n, n in ['AND', 'OR'])
+                            if n in ['AND', 'OR']:
+                                new_label = '(' + self.ph_stack[phkey].label() + ')'
+                            else:
+                                new_label = self.ph_stack[phkey].label()
+                                print('new label', new_label)
+                            label = re.sub(ph_key_match, new_label, label)
+                            #print('new label match', old_label, ' => ', label)
+                    
             #print('shard', key, c.label(), c)#
             r.append((label, c))
 
+        #print('all labels', all_labels)
         # Sort based on label length
         r = sorted(r, key=lambda x: len(x[0]))
         #print('rrrrrrr', r)
@@ -217,15 +277,26 @@ class Parse(object):
             #print(label, ',', c, ':', c.has_ph)
 
         # Finally, add on overall thing
-        # Fix label on this
-        r.append((self.s.label(), self.s))
+        r.append((self.resolve_label(self.s.label()), self.s))
         return r
 
-    def resolve_label(self, k, kz=None):
-        pass
+    def resolve_label(self, s):
+        # Just for like the column headers basically
+        ph_key_match = r"(\$ph\d+)"
+        while True:
+            if '$ph' not in s:
+                break
+            else:
+                m = self.match(ph_key_match, s, simple=False)
+                for phkey in m:
+                    if phkey.startswith('$ph'):
+                        new_label = '(' + self.ph_stack[phkey].label() + ')'
+                        s = re.sub(re.escape(phkey), new_label, s)
+        return s
 
     def binstrappend(self, kz):
         # return all the calculated columns
+        #print('Stack', self.ph_stack)
         binstr = []
         for c in self.components():
             val = c[1].compute(kz, ph=self.ph_stack)
